5970e8a693683e748ab64b733c922fe446271720
jpg27 <juanpablog01@gmail.com>
2024-04-30 00:41:31 -0500

    clase 26 abril

---
+++ src/main/java/co/edu/uco/pch/crosscutting/exceptions/custom/DataPCHException.java
@@ -0,0 +1,20 @@
+package co.edu.uco.pch.crosscutting.exceptions.custom;
+
+import co.edu.uco.pch.crosscutting.exceptions.PCHException;
+import co.edu.uco.pch.crosscutting.exceptions.enums.Lugar;
+
+public final class DataPCHException extends PCHException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DataPCHException(final String mensajeUsuario) {
+		super(mensajeUsuario, Lugar.DATA);
+	}
+	
+	public DataPCHException(final String mensajeTecnico,final String mensajeUsuario,final Throwable excepcionRaiz) {
+		super(mensajeTecnico, mensajeUsuario, Lugar.DATA, excepcionRaiz);
+	}
+
+
+
+}
--- src/main/java/co/edu/uco/pch/crosscutting/helpers/ObjetHelper.java
+++ src/main/java/co/edu/uco/pch/crosscutting/helpers/ObjetHelper.java
@@ -1,5 +1,24 @@
 package co.edu.uco.pch.crosscutting.helpers;
 
-public class ObjetHelper {
+public final class ObjetHelper {
+	
+	private static final ObjetHelper INSTANCE = new ObjetHelper();
+	
+	private ObjetHelper() {
+		super();
+	}
+	
+	public static final ObjetHelper getObjetHelper() {
+		return INSTANCE;
+	}
+	
+	public <O> boolean isNull(O objeto) {
+		return objeto == null;
+	}
+	
+	public <O> O getDefaultValue(O objeto, O valorDefecto) {
+		return isNull(objeto)? valorDefecto: objeto;
+	}
+
 
 }
